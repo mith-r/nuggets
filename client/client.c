4d785a3566e7a104f62c537c3e1466f03d5b094b
Tarini Gupta <f0079sp@plank.thayer.dartmouth.edu>
2025-05-28 16:08:38 -0400

    saved client.c

--- client/client.c
+++ client/client.c
@@ -5,16 +5,15 @@
  * CS50, Spring 2025
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <ctype.h>
-#include <ncurses.h>
-/*#include "grid.h"     
-#include "player.h"  
-#include "log.h"*/
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <ctype.h>
+ #include <ncurses.h>
+ #include <stdbool.h>
+ #include <signal.h>
+ #include "../support/message.h"
+ #include "../support/log.h"
 
 #define MaxNameLength = 50;   // max number of chars in playerName
 #define MaxPlayers = 26;      // maximum number of players
@@ -22,117 +21,19 @@
 #define GoldMinNumPiles = 10; // minimum number of gold piles
 #define GoldMaxNumPiles = 30; // maximum number of gold piles
 
-/************ global types ************/
-typedef struct player{
-    char letter;
-    int address;
-    char name;
-    int purse;
-    map_t playerMap;
-    int posX;
-    int posY;
-} player_t;
-
 
 /************ function prototypes ************/
-static void parseArgs(int argc, char *argv[], addr_t *server_addr);
-bool connectClient(char* hostname, char* port, addr_t* server_addr);
-void initDisplay(addr_t* server, char* playerName);
-static bool handleMessage(void *arg, const addr_t from, const char *message);
-bool createWindow(char *args);
-bool displayGrid(char* grid);
-static bool handleInput(void *arg);
-static void quitQame(char *message);
+static bool verifyArgs(const int argc, const char* argv[]);
+static void initializeDisplay(int nrows, int ncols);
+static void quitGame(const char* input);
+static void serverComs(const char* serverHost, const char* serverPort);
+static bool handleInput(void* arg);
+void sendToServer(addr_t to, char* input);
+static bool handleMessage(void* arg, const addr_t addr, const char* input);
+static void showDisplay(void);
+static void resize(int i);
+void checkWinSize(void);
+static void readGold(const char* input);
+static void readGrid(const char* input);
+static void cleanGame(void);
 
-player_t* player_new(char* username, char letter, int x, int y) {
-    
-    player_t* newPlayer = malloc(sizeof(player_t));
-
-    newPlayer -> username = username;
-    newPlayer->letter = letter;
-    newPlayer -> x = x;
-    newPlayer->y = y;
-
-    return newPlayer;
-}
-
-
-int player_getX(player_t* player) {
-
-    if (player == NULL) {
-        return;
-    }
-
-    return player->x;
-}
-
-int player_getY(player_t* player) {
-
-    if (player == NULL) {
-        return;
-    }
-
-    return player->y;
-}
-
-
-//gets how much gold a player has
-int player_getGold(player_t* player) {
-
-    if (player == NULL) {
-        flog_v(stderr, "Could not get gold from NULL player");
-        return;
-    }
-
-    return player->gold;
-}
-
-
-//adding gold to a player
-static void player_depositGold(player_t* player, int gold) {
-
-    if (player == NULL) {
-        flog_v(stderr, "Could not deposit gold to NULL player");
-        return;
-    }
-
-    player->gold += gold;
-}
-
-
-
-//gets the letter assigned to the player
-char player_getLetter(player_t* player) {
-
-    if (player == NULL) {
-        flog_v(stderr, "Could not get letter from NULL player");
-        return;
-    }
-
-    return player->letter;
-}
-
-
-//gets the username of a player
-char* player_getUsername(player_t* player) {
-
-    if (player == NULL) {
-        flog_v(stderr, "Could not get username from NULL player");
-        return;
-    }
-
-    return player->username;
-
-}
-
-
-//deletes a player
-void player_delete(player_t* player) {
-    
-    if (player == NULL) {
-        return;
-    }
-
-    free(player->username);
-    free(player);
-}
