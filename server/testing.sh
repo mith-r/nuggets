b36a41f456ecfa6bce87908ee7fb18eae1d18bff
Ben Lin <blin97198@gmail.com>
2025-06-02 15:10:37 -0400

    changed testing.sh

--- server/testing.sh
+++ server/testing.sh
@@ -1,18 +1,15 @@
 #!bin/bash
-
+#testing.sh to test invalid arguments for server
 
 # Testing with no arguments
 ./server
 
 # Testing with more than 2 arguments
-./server maps/main.txt 10 hello
+./server ../maps/main.txt 10 hello
 
 #Testing server with invalid mapFile
-./server maps/hello.txt
+./server ../maps/hello.txt
 
 #Testing server with invalid port
-./server maps/main.txt -1
-./server maps/main.txt hi
-
-#Testing server with optional port
-./server maps/main.txt
+./server ../maps/main.txt -1
+./server ../maps/main.txt hi
