4d5c2eff4cf6f60a239e27f55f45575324d48f46
Ben Lin <blin97198@gmail.com>
2025-06-04 03:09:42 -0400

    fixed players showing up when joining

--- server/server.c
+++ server/server.c
@@ -716,7 +716,7 @@ char* displayGame(game_t* game, addr_t fromClient) {
             }
 
             //if ANOTHER player is at that point, display their letter
-            else if (playerLetter != ' ' && playerLetter != '@') {
+            else if (playerLetter != ' ' && playerLetter != '@' && point_getVisibility(localPoint)) {
               mapSymbol = playerLetter;
             }
 
